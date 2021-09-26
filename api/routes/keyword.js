81ed424f4fb874d8da82d323766eca5adbc3fd16
aukey2 <aukey2@illinois.edu>
2021-09-26 16:16:23 -0500

    Wiki info in KeywordPane

--- api/routes/keyword.js
+++ api/routes/keyword.js
@@ -1,11 +1,48 @@
 const express = require("express");
 const Keyword = require("../models/keyword");
-const { logSqlError } = require("../utils");
+const wiki = require("wikijs").default();
 
 const router = express.Router();
 
+const NUM_WIKI_RESULTS = 15;
+
 router.get("/", async (req, res) => {
   const keyword = await Keyword.get(req.query.id);
+  keyword.wiki = {};
+
+  try {
+    // Fetch article info for keyword
+    let wikiPage = await wiki.page(keyword.name);
+
+    const pageUrl = await wikiPage.url();
+    wikiPage = await wikiPage.chain().summary().request();
+
+    keyword.wiki.title = wikiPage.title;
+    keyword.wiki.summary = wikiPage.extract;
+    keyword.wiki.mainUrl = pageUrl;
+  } catch (e) {
+    // No wikipedia article found with keyword's name
+    if (e.message === "No article found") {
+      console.log(e.message);
+    } else return console.error(e);
+
+    // Fetch search results
+    const wikiSearch = await wiki.search(keyword.name, NUM_WIKI_RESULTS, true);
+    let wikiSearchResults = wikiSearch.results;
+
+    const wikiPageIdUrl = "http://en.wikipedia.org/?curid=";
+    wikiSearchResults = wikiSearchResults.map((res) => ({
+      title: res.title,
+
+      // Convert wiki pageids to corresponding url
+      url: wikiPageIdUrl + res.pageid,
+    }));
+
+    const searchQuery = encodeURIComponent(keyword.name).replace("%20", "+");
+    keyword.wiki.mainUrl = "https://en.wikipedia.org/?search=" + searchQuery;
+    keyword.wiki.search = wikiSearchResults;
+  }
+
   res.send(keyword);
 });
 
--- app/src/components/KeywordPane.js
+++ app/src/components/KeywordPane.js
@@ -7,9 +7,26 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   googleIcon: {
-    marginLeft: 12,
-    height: 20,
-    width: 20,
+    height: 24,
+    marginLeft: 13,
+    marginTop: 3,
+  },
+  wikiIcon: {
+    marginRight: 12,
+    height: 40,
+  },
+  wikiTitleContainer: {
+    display: "flex",
+    alignItems: "center",
+  },
+  wikiSummaryText: {
+    fontSize: 12,
+  },
+  wikiSummaryContainer: {
+    marginTop: 8,
+    maxHeight: 175,
+    overflowY: "scroll",
+    paddingRight: 4,
   },
   container: {
     backgroundColor: theme.palette.backGray.main,
@@ -18,22 +35,28 @@ const useStyles = makeStyles((theme) => ({
     borderColor: theme.palette.pendingYellow.main,
     flex: 1,
     flexDirection: "column",
-    padding: "25px 15px",
+    paddingTop: 20,
+    paddingLeft: 15,
+    paddingRight: 5,
     marginLeft: 10,
   },
   titleContainer: {
     display: "flex",
   },
   titleText: {
-    fontSize: 16,
+    fontSize: 18,
+    wordBreak: "break-all",
+    hyphens: "auto",
   },
   infoContainer: {
     display: "flex",
     flexDirection: "column",
     height: "100%",
-    marginTop: 10,
+    marginTop: 20,
   },
-  infoText: { fontSize: 14, marginTop: 10 },
+  infoTitle: {
+    fontSize: 14,
+  },
 }));
 
 export default function KeywordPane(props) {
@@ -61,37 +84,57 @@ export default function KeywordPane(props) {
   const googleSearchQuery = encodeURIComponent(keyword?.name);
   const googleSearchUrl = "http://www.google.com/search?q=" + googleSearchQuery;
 
-  return (
+  return !keyword ? null : (
     <Box className={classes.container}>
-      {keyword?.name && (
-        <div className={classes.titleContainer}>
-          <Typography className={classes.titleText}>
-            {
-              // <u>{keyword.name}</u>
-            }
-            {keyword.name}
-          </Typography>
-          <TransparentButton href={googleSearchUrl} target="_blank">
-            <img
-              className={classes.googleIcon}
-              src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-            />
-          </TransparentButton>
-        </div>
-      )}
-
-      <div className={classes.infoContainer}>
-        {keyword?.id && (
-          <Typography className={classes.infoText}>
-            <b>Id:</b> {keyword.id}
-          </Typography>
-        )}
-        {keyword?.definition && (
-          <Typography className={classes.infoText}>
-            <b>Definition:</b> {keyword.definition}
-          </Typography>
-        )}
+      <div className={classes.titleContainer}>
+        <Typography className={classes.titleText}>{keyword.name}</Typography>
+        <TransparentButton href={googleSearchUrl} target="_blank">
+          <img
+            className={classes.googleIcon}
+            src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+          />
+        </TransparentButton>
       </div>
+      <div className={classes.infoContainer}>
+        <TransparentButton href={keyword.wiki.mainUrl} target="_blank">
+          <div className={classes.wikiTitleContainer}>
+            <img
+              className={classes.wikiIcon}
+              src="https://upload.wikimedia.org/wikipedia/en/8/80/Wikipedia-logo-v2.svg"
+            />
+
+            <Typography className={classes.infoTitle}>
+              <b>Wikipedia</b>{" "}
+              {keyword.wiki.search ? "(search results)" : "(article)"}
+            </Typography>
+          </div>
+        </TransparentButton>
+
+        <div className={classes.wikiSummaryContainer}>
+          {keyword.wiki.summary && (
+            <Typography className={classes.wikiSummaryText}>
+              {keyword.wiki.summary}
+            </Typography>
+          )}
+          {keyword.wiki.search && (
+            <ul>
+              {keyword.wiki.search.map((res) => (
+                <li>
+                  <TransparentButton href={res.url} target="_blank">
+                    {res.title}
+                  </TransparentButton>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+
+      {keyword.definition && (
+        <Typography className={classes.infoText}>
+          <b>Definition:</b> {keyword.definition}
+        </Typography>
+      )}
     </Box>
   );
 }
